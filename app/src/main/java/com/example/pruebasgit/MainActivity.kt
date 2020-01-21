d2f5e330a7c2f9f59d8e53d30ca4b3d39d71e4d7
JulianArmengol <julianga96@gmail.com>
2020-01-21 16:52:28 -0600

    Add login implementation

--- app/src/main/java/com/example/pruebasgit/MainActivity.kt
+++ app/src/main/java/com/example/pruebasgit/MainActivity.kt
@@ -2,11 +2,14 @@ package com.example.pruebasgit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Login Branch", Toast.LENGTH_SHORT).show();
     }
 }
